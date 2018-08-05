9d4edcd1a3237e1dcbcc61ac13b4203b88bb97b1
Allen Nguyen <allendewy@gmail.com>
2018-08-05 16:57:05 -0400

    pom configuration and empty project

---
+++ src/main/java/com/revature/assignforce/assignforcefilehandlerservice/AssignForceFilehandlerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.revature.assignforce.assignforcefilehandlerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignForceFilehandlerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignForceFilehandlerServiceApplication.class, args);
+    }
+}
